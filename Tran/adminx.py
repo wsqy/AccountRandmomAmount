4aabac78231937c0c6f82cb6644485e23ae07813
qiyuan <1323800521@qq.com>
2020-06-16 11:13:21 +0800

    日常交易管理后台调整完成

--- Tran/adminx.py
+++ Tran/adminx.py
@@ -1,8 +1,6 @@
 import xadmin
 from django.utils.html import format_html
-
 from .models import  Task, TaskBatch, Transaction
-
 
 class TaskAdmin:
     def download(self, obj):
@@ -10,8 +8,6 @@ class TaskAdmin:
         return format_html(button_html)
     download.short_description = '下载报表'
 
-    add_form_template = 'xadmin/Tran/task/model_form.html'
-    change_form_template = add_form_template
     list_display = ['date', 'batch_total', 'remark', 'status', 'download']
     readonly_fields = ['status',]
     empty_value_display = '无'
@@ -22,18 +18,59 @@ class TaskAdmin:
             instance.name = instance.date.strftime('%Y-%m-%d任务')
         super(TaskAdmin, self).save_models()
 
+    def get_context(self):
+        context = super(TaskAdmin, self).get_context()
+        context.update({
+            'show_save_as_new': False, 
+            'show_save_and_continue': False,
+            'show_save_and_add_another': False,
+            'show_delete_link': False,
+        })
+        return context
+
+    def get_readonly_fields(self, obj=None):
+        if 'update' in self.request.path:
+            return  ['amount_total_min', 'amount_total_max', 'batch_num_min', \
+                     'batch_num_max', 'batch_total','status','remark', 'download_link']
+
+
 class TaskBatchAdmin:
     list_display = ['task', 'num', 'batch_total', 'amount_total', 'remark', ]
     list_filter = ['task', ]
-    empty_value_display = '无'
-
-class TransactionAdmin:
-    list_display = ['task', 'task_batch', 'buyer', 'seller', 'remark', ]
-    list_filter = ['task', 'task_batch', ]
+    readonly_fields = ['task', 'num', 'batch_total', 'amount_total', 'remark',]
     empty_value_display = '无'
 
     def has_add_permission(self):
         return False
+
+    def get_context(self):
+        context = super(TaskBatchAdmin, self).get_context()
+        context.update({
+            'show_save': False,
+            'show_save_as_new': False, 
+            'show_save_and_continue': False, 
+            'show_delete_link': False,
+        })
+        return context
+
+class TransactionAdmin:
+    list_display = ['task', 'task_batch', 'buyer', 'seller', 'amount', 'remark', ]
+    list_filter = ['task', 'task_batch', ]
+    readonly_fields = ['task', 'task_batch', 'buyer', 'seller', 'amount', 'remark', ]
+    empty_value_display = '无'
+
+    def has_add_permission(self):
+        return False
+
+    def get_context(self):
+        context = super(TransactionAdmin, self).get_context()
+        context.update({
+            'show_save': False,
+            'show_save_as_new': False, 
+            'show_save_and_continue': False, 
+            'show_delete_link': False,
+        })
+        return context
 
 xadmin.site.register(Task, TaskAdmin)
 xadmin.site.register(TaskBatch, TaskBatchAdmin)
