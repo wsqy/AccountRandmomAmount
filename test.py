6fd172020b8f15b19adaffa8f798313c0935cad7
qiyuan <1323800521@qq.com>
2020-08-14 13:35:35 +0800

    修改部分隐藏的供应商状态置为否

--- test.py
+++ test.py
@@ -1,22 +1,8 @@
-from Tran.utils import hongbao
-
-def test2():
-  nums = 0
-  while True:
-      if nums > 10:
-          return 
-      nums += 1
-      hongbao_list = hongbao(total=2146, num=4)
-      # print('-hongbao_list--')
-      # print(hongbao_list)
-      if (max(hongbao_list) < 950) and (min(hongbao_list) > 50):
-          return hongbao_list
-
+from Account.models import Seller
 
 def test():
-  for i in range(100):
-    hongbao_list = test2()
-    if max(hongbao_list) > 950 or min(hongbao_list) < 50:
-      print(sum(hongbao_list))
-      print(hongbao_list)
-      print('-------')
+  with open('橡塑隐藏取消.txt',) as f:
+    for (i,line) in enumerate(f.readlines(), 1):
+      # print("%s %s" % (i, line))
+      Seller.objects.filter(name=line.strip('\n').strip()).update(is_activate=False)
+
